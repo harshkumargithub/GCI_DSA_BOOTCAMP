cd9f555469a6ea58202fee1809c99a4b8f5c2e67
harshkumargithub <83367701+harshkumargithub@users.noreply.github.com>
2022-03-25 23:15:45 +0530

    Add files via upload

---
+++ MileStone04/23.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, num;
+    unsigned long long fact=1LL;
+
+    printf("Enter number to calculate factorial: ");
+    scanf("%d", &num);
+
+    for(i=1; i<=num; i++)
+    {
+        fact = fact * i;
+    }
+    printf("Factorial of %d = %llu", num, fact);
+
+    return 0;
+}
