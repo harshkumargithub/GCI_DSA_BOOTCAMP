6f7993fc5d941b7828838e2118ff008d9a28259e
harshkumargithub <83367701+harshkumargithub@users.noreply.github.com>
2022-04-30 23:28:52 +0530

    Add files via upload

---
+++ MileStone09/12.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i,j,k;
+    for(i=5; i>=1; i--)
+    {
+        if(i%2==1) k=1;
+        else k=i;
+        for(j=1; j<=i; j++)
+        {
+            cout<<k;
+            if(i%2==1) k++;
+            else k--;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
