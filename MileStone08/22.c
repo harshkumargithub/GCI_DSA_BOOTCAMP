41ba358772ef81867e2c8ad348d9bcad83ac1aff
harshkumargithub <83367701+harshkumargithub@users.noreply.github.com>
2022-04-30 23:27:03 +0530

    Add files via upload

---
+++ MileStone08/22.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, k;
+    for (i = 1; i <= 4; i++)
+    {
+        k = 49;
+        for (j = 1; j <= 7; j++)
+        {
+            if (j >= 5 - i && j <= 3 + i)
+            {
+                printf("%c", k);
+                k++;
+                if (j == 5)
+                    k = 65;
+            }
+            else
+                printf("");
+        }
+        printf("\n");
+    }
+}
